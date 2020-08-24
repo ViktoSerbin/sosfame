a54ac23947c8be608c01fd0d80d56c2296314d3b
ViktoSerbin <taur1721@gmail.com>
2020-08-24 20:44:12 +0300

    end project

---
+++ app/js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+
+  $('.slider').slick({
+    prevArrow: '<button class="slider-btn slider-btn__left"><svg width="18" height="39" viewBox="0 0 18 39" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.82153 2.65635L1.82153 36.7297L15.5642 19.8838L1.82153 2.65635Z" stroke="white" stroke-width="3.36454" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+    nextArrow: '<button class="slider-btn slider-btn__right"><svg width="18" height="39" viewBox="0 0 18 39" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.82153 2.65635L1.82153 36.7297L15.5642 19.8838L1.82153 2.65635Z" stroke="white" stroke-width="3.36454" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        }
+      },
+    ]
+  });
+
+  $('.features__info-title').on('click', function () {
+    $(this).children('.features__drop-down').toggleClass('active');
+  });
+
+  $('.header__menu-btn').on('click', function () {
+    $(this).toggleClass('header__menu-btn-active');
+  });
+
+  $('.header__menu-btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
+
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('html', function(){
 });
 
 gulp.task('js', function(){
-    return qulp.src('app/js/*.js')
+    return gulp.src('app/js/*.js')
             .pipe(browserSync.reload({stream:true}))
 });
 
@@ -57,8 +57,8 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
